b00feac1a520f79f64e2dd529c498d568f0a836f
jinhyuk9714 <jinhyuk9714@gmail.com>
2024-08-05 21:13:17 +0900

    Like 기능 (PostController)

--- src/main/java/backend/backend/controller/PostController.java
+++ src/main/java/backend/backend/controller/PostController.java
@@ -8,18 +8,18 @@ import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequiredArgsConstructor
-@RequestMapping("/api/post/{id}/like")
-public class LikesController {
+@RequestMapping("/api/post")
+public class PostController {
 
     private final LikesService likesService;
 
-    @PostMapping
+    @PostMapping("/{id}/like")
     public ApiResponse<Void> likePost(@PathVariable Long id, @RequestBody LikesRequestDto likesRequestDto) {
         likesService.likePost(id, likesRequestDto);
         return ApiResponse.success(null, "좋아요가 성공적으로 추가되었습니다.");
     }
 
-    @DeleteMapping
+    @DeleteMapping("/{id}/like")
     public ApiResponse<Void> unlikePost(@PathVariable Long id, @RequestBody LikesRequestDto likesRequestDto) {
         likesService.unlikePost(id, likesRequestDto);
         return ApiResponse.success(null, "좋아요가 성공적으로 취소되었습니다.");
